07f2cdb7ef8e5216babd730c722af667d487b0a1
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-04-27 16:03:58 +0200

    - moved code - added solution 2

---
+++ 02_reverse_lines/reverse_lines.rb
@@ -0,0 +1,10 @@
+unless ARGV.size == 0
+  filename = ARGV[0]
+  lines = []
+  File.open(filename, "r") do |file|
+    file.each_line{ |line| lines << line }
+  end
+  puts lines.reverse.join
+else
+  puts "usage: ruby reverse_lines.rb [filename]\nplease give the filename to reverse"
+end
