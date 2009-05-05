40196008a3fac6e5ca2790a9ae9bbc52b06059fe
HSR <hsr@hsrs-macbook.local>
2009-05-05 09:25:35 +0200

    removed ugly code.

--- 06_number_check/numbercheck.rb
+++ 06_number_check/numbercheck.rb
@@ -1,33 +1,3 @@
-class RegexpOr
-  def initialize(r1,r2)
-    @r1, @r2 = r1, r2
-  end
-  
-  def =~(str)
-    @r1 =~ str or @r2 =~ str
-  end
-end
-
-class RegexpAnd
-  def initialize(r1,r2)
-    @r1, @r2 = r1, r2
-  end
-  
-  def =~(str)
-    @r1 =~ str and @r2 =~ str
-  end
-end
-
-class Regexp
-  def |(other)
-    RegexpOr.new(self, other)
-  end
-  
-  def &(other)
-    RegexpAnd.new(self, other)
-  end
-end
-
 class Numbercheck
   
   SIGN = /^[-|+]{0,1}/
