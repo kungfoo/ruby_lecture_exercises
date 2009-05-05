7d1a22c73787791a87f4a2311baebb75ca317dda
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-05-05 18:46:08 +0200

    added small file that prints the imports nicely.

---
+++ 07_import_filter/run_import_filter.rb
@@ -0,0 +1,9 @@
+require "import_filter"
+
+dir = "java code"
+
+filter = ImportFilter.new(dir)
+puts "imports used by files in #{dir}\n" << "-"*50
+filter.imports.each do |import|
+  puts import
+end
