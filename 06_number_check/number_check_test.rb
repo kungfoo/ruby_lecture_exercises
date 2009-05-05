00a23c8f7285b914867aef7813224a5a3bd939b1
HSR <hsr@hsrs-macbook.local>
2009-05-05 09:21:44 +0200

    experimental version of RegexpAnd and Or added. don't like it right now.

---
+++ 06_number_check/number_check_test.rb
@@ -0,0 +1,9 @@
+require "test/unit"
+
+require "numbercheck"
+
+class TestNumbercheck < Test::Unit::TestCase
+  def test_simple_numbers
+    
+  end
+end
--- 06_number_check/numbercheck.rb
+++ 06_number_check/numbercheck.rb
@@ -1,8 +1,42 @@
+class RegexpOr
+  def initialize(r1,r2)
+    @r1, @r2 = r1, r2
+  end
+  
+  def =~(str)
+    @r1 =~ str or @r2 =~ str
+  end
+end
+
+class RegexpAnd
+  def initialize(r1,r2)
+    @r1, @r2 = r1, r2
+  end
+  
+  def =~(str)
+    @r1 =~ str and @r2 =~ str
+  end
+end
+
+class Regexp
+  def |(other)
+    RegexpOr.new(self, other)
+  end
+  
+  def &(other)
+    RegexpAnd.new(self, other)
+  end
+end
+
 class Numbercheck
   
-  @@normal = /[0-9]*.[]/
+  SIGN = /^[-|+]{0,1}/
+  NORMAL = /^[0-9]+.[0.9]*/
+  SCIENTIFIC = //
+  FORMAT = (SIGN & NORMAL) | (SIGN & SCIENTIFIC) 
   
   def self.valid?(string)
+    copy = String.new(string)
     
   end
 end
