29d15715538daee7432a258c05aefcaad0d82155
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-05-06 09:47:25 +0200

    nicer output.

--- 07_import_filter/run_import_filter.rb
+++ 07_import_filter/run_import_filter.rb
@@ -2,7 +2,7 @@ require "import_filter"
 
 dir = "java code"
 filter = ImportFilter.new(dir)
-puts "imports used by files in #{dir}\n" << "-"*50
+puts "imports used by files in '#{dir}'\n" << "-"*50
 
 filter.imports.each do |import|
   puts import
