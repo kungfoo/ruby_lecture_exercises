4dc47cbda3c6bb758009d82e1b17e6e49e9cf8d2
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-05-06 15:19:17 +0200

    corrected indentation.

--- 08_time_format/time_parser.rb
+++ 08_time_format/time_parser.rb
@@ -3,12 +3,12 @@ class TimeParser
   MINUTES = /^\d{2}\s*/
   AM_PM = /^(am|pm|AM|PM)$/
   FORMAT = [HOURS, MINUTES, AM_PM]
-  
+
   HOURS_RANGE = (0..12)
   MINUTES_RANGE = (0..59)
-  
+
   attr_reader :hours, :minutes
-  
+
   def self.valid?(string)
     copy = String.new(string)
     FORMAT.each do |part|
@@ -20,7 +20,7 @@ class TimeParser
     end
     return true
   end
-  
+
   def initialize(string)
     raise(ArgumentError, 'The format is not valid') unless TimeParser.valid?(string)
     string = String.new(string)
@@ -28,7 +28,7 @@ class TimeParser
     parse_minutes(string)
     parse_am_pm(string)
   end
-  
+
   def parse_hours(string)
     match = string.match(HOURS)
     string.gsub!(HOURS, "")
@@ -36,22 +36,22 @@ class TimeParser
     @hours = matched_string.to_i
     raise(ArgumentError, 'The hours should be in (0..12)') unless HOURS_RANGE === @hours
   end
-  
+
   def parse_minutes(string)
     match = string.match(MINUTES)
     string.gsub!(MINUTES, '')
     @minutes = match.to_s.to_i
     raise(ArgumentError, 'Minutes should be in (0..59)') unless MINUTES_RANGE === @minutes
   end
-  
+
   def parse_am_pm(string)
     case string
-      when /(pm|PM)/
-        @hours += 12 if @hours < 12
-      when /(am|AM)/
-        @hours = 0 if @hours == 12
-      else
-        raise('something is terribly wrong with the code...')
+    when /(pm|PM)/
+      @hours += 12 if @hours < 12
+    when /(am|AM)/
+      @hours = 0 if @hours == 12
+    else
+      raise('something is terribly wrong with the code...')
     end
   end
 end
